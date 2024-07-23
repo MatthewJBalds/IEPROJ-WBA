36d16f70b1180f7a3f6e874036e24572da733297
Mathieu Marc Pobre <mathieu_marc_pobre@dlsu.edu.ph>
2024-07-23 08:14:25 +0800

    Health Bar text value

--- IEPROJ/Assets/HealthBar.cs
+++ IEPROJ/Assets/HealthBar.cs
@@ -7,14 +7,19 @@ public class HealthBar : MonoBehaviour
 {
 
     public Slider slider;
-   
+    public Text hpText;
+
+    private int maxHealth;
     public void setMaxHealth(int health)
     {
+        maxHealth = health;
+        hpText.text = string.Format("{0:0}/{1:0}", health, maxHealth);
         this.slider.maxValue = health;
         this.slider.value = health;
     }
     public void setHealth(int health)
     {
+        hpText.text = string.Format("{0:0}/{1:0}", health, maxHealth);
         this.slider.value = health;
     }
 }
